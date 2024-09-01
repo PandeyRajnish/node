add21b1a1e95fe765cd8ae99a65d7a9ec0923f7a
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2024-09-01 11:17:14 +0530

    cleaned everything

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const lib = require("./lib");
-// import { sum, diff } from "./lib.js";
-// const fs = require("fs");
 
-// const t1 = performance.now(); // on which time this line got executed
-// const text = fs.readFileSync('demo.txt', 'utf8');
-
-// const text = fs.readFile("demo.txt", "utf8", (error, text) => {
-//   console.log(text);
-// });
-// console.log(text)
-
-// console.log(lib.sum(4, 5), lib.diff(4, 5));
-// const t2 = performance.now();
-// a = 5;
-// console.log(t2 - t1);
