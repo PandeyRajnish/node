bcd9e92c94b844b9aa361812bd0bf197ae9b2b0a
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2024-09-01 12:27:00 +0530

    sent static files in response

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const http = require("http");
 
+const data = {
+  name: "Rajnish Pandey",
+};
+
+const fs = require("fs");
+
+const index = fs.readFileSync("index.html", "utf8");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log("server started");
+  res.setHeader("Dummy", "Dummy Value");
+  res.setHeader("Content-Type", "text/html");
+  res.end(index);
+});
+
+server.listen(8080);
